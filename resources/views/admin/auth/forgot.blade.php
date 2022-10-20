c5444aa4cb9828e1008d9a345eea0a6239321881
Manalo089884 <Manalo.089884@munoz.sti.edu.ph>
2022-10-20 16:45:16 +0800

    la

--- resources/views/admin/auth/forgot.blade.php
+++ resources/views/admin/auth/forgot.blade.php
@@ -7,7 +7,7 @@
             <a href="#" class="-intro-x flex items-center pt-5">
                 <img alt="Go Dental Logo" class="w-6" src="{{asset('icons/log.png')}}">
                 <span class="text-white text-lg ml-3">Go Dental</span>
-            </a>
+            </a> 
             <div class="my-auto">
                 <img alt="Go Dental Image " class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/login_shop.svg') }}">
                 <div class="-intro-x text-white font-medium text-3xl leading-tight mt-10"> Create a new password for {{$email}}</div>
